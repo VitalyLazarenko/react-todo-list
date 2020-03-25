6c4bd5725419e9c4b633a4a68f0ee287bf093302
VitalyLazarenko <hristofor.xxxx@gmail.com>
2020-03-25 14:49:44 +0200

    refactoring project. Add component app. Add project style.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,7 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import AppHeader from "./components/app-header";
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
+import App from "./components/app/app";
 
-const App = () => {
-
-    return (
-        <div>
-            <AppHeader/>
-            <SearchPanel/>
-            <TodoList/>
-        </div>
-    )
-};
-
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<App />,
+    document.getElementById('root'));
